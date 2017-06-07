1b059694f8eea10fac513bfa184917956cf2af90
Asus <Asus@DESKTOP-CKKRGI8>
2017-06-07 21:25:32 +0300

    Реализован сервер для чата

---
+++ Comstar.Client/Comstar.Client/Models/ChatRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Comstar.Client.Models
+{
+	public class ChatRecord : BaseDataObject
+	{
+		private string _author;
+
+		private string _message;
+
+		private DateTime _timestamp;
+
+		public string Author { get => _author; set => SetProperty(ref _author, value); }
+
+		public string Message { get => _message; set => SetProperty(ref _message, value); }
+
+		public DateTime Timestamp { get => _timestamp; set => SetProperty(ref _timestamp, value); }
+	}
+}
---
+++ Comstar.Server/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Comstar.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Comstar.Server.Controllers
+{
+	[Route("api/[controller]")]
+	public class ChatController : Controller
+	{
+		private static List<ChatRecord> _chatRecords = new List<ChatRecord>
+		{
+			new ChatRecord
+			{
+				Id = Guid.NewGuid().ToString(),
+				Author = "Вася",
+				CreatedAt = DateTime.Now.AddMinutes(-10),
+				Message = "Привет, всем!"
+			},
+			new ChatRecord
+			{
+				Id = Guid.NewGuid().ToString(),
+				Author = "Петя",
+				CreatedAt = DateTime.Now.AddMinutes(-9),
+				Message = "Привет, Вася!"
+			}
+		};
+
+		[HttpGet]
+		public IEnumerable<ChatRecord> Get()
+		{
+			return _chatRecords;
+		}
+
+		[HttpPost]
+		public void Post([FromBody]ChatRecord value)
+		{
+			_chatRecords.Add(value);
+		}
+	}
+}
